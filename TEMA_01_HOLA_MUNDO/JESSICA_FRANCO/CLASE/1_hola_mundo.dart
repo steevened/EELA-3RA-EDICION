7ef29bdbed21c103ec419a8dde038df191c1e804
JFranco0914 <jfranco0914@gmail.com>
2024-07-29 20:59:11 -0500

    Add : files CLASE : 1_hola_mundo.dart, 2_comentarios.dart TAREA : hola_mundo.dart

---
+++ TEMA_01_HOLA_MUNDO/JESSICA_FRANCO/CLASE/1_hola_mundo.dart
@@ -0,0 +1,5 @@
+void main(){
+  //Cuerpo de la función
+  print("Hola mundo");
+  print("Hola Jessy ♥ \nY nos fuimos pa' bajo");
+}
---
+++ TEMA_01_HOLA_MUNDO/JESSICA_FRANCO/TAREA/hola_mundo.dart
@@ -0,0 +1,12 @@
+// ## 1. Teoría
+
+
+
+
+// 2. Análisis
+// No tiene la palabra reservada void para iniciar el main
+// Las líneas de código DEBEN terminar en ";"
+// La instrucción correcta es "print" nooo "println"
+void main() {
+    print("Hello World");
+}
