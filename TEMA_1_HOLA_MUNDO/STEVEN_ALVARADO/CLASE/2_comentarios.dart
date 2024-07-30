5c6f0eef01fac824b8b5d30f087351da400c7ef2
steevened <alvaradosteven315@gmail.com>
2024-07-29 20:44:45 -0500

    tarea

---
+++ TEMA_1_HOLA_MUNDO/STEVEN_ALVARADO/CLASE/2_comentarios.dart
@@ -0,0 +1,18 @@
+void main() {
+  // first comment
+
+/*
+  block comments
+  */
+  saludo();
+}
+
+///
+/// # Este es un comentario de documentación
+/// <h2> Esto es un H2 </h2>
+///
+/// Mas texto que puede servir para identificar algo
+///
+void saludo() {
+  print("greetings");
+}
---
+++ TEMA_1_HOLA_MUNDO/STEVEN_ALVARADO/TAREA/hola_mundo.dart
@@ -0,0 +1,18 @@
+// ## 1. Teoría
+
+// En clase vamos a comentar mediante el chat, todas las características que encontramos en el lenguaje dart:
+
+// * Fuertemente tipado
+// * ¿Que otras conocemos?
+// * Permite comentarios de una o múltiples líneas
+// * Permite comentarios de documentación
+
+// ```dart
+//     *main() {
+//         println("Hello World")
+//     }
+// ```
+
+main() {
+  print("Hello World");
+}
