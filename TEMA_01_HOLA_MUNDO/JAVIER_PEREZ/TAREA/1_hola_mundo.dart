7675d78b834ac5778406ff50ff134baa2c4f9176
JAvierPerez777 <javiperezizu@gmail.com>
2024-07-30 15:54:13 -0500

    add: files, tarea_clase_hola_mundo

---
+++ TEMA_01_HOLA_MUNDO/JAVIER_PEREZ/TAREA/1_hola_mundo.dart
@@ -0,0 +1,34 @@
+/* Tarea
+   
+Tema: Hola Mundo
+
+Características que encontramos en el lenguaje dart:
+
+1. ES fuertemente tipado, quiere decir que los tipos de datos tienen que ser explicitamente declarados
+2. Se encuentra orientado a objetos
+3. Se puede visualizar los cambios al instante
+4. Se ejecuta en multiples plataformas
+*/
+
+/* Analisis codigo: 
+
+main (){
+  println("Hello World");
+}
+
+El codigo tiene error de sintaxis en lo siguiente:
+a. Se omite la palabra void
+b. Se incorpora las letras ln (println) que para este lenguaje no se utiliza
+c. Se omite el ";" importante para la ejecución del código
+
+A continuación el ejemplo correcto.
+
+void main (){
+  print("Hello World"); 
+}
+
+*/
+
+void main (){
+  print("Hello World");
+}
