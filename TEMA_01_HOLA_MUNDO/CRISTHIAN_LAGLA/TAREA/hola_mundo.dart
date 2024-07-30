a44e3d5e0241526d6707379140b0dda38a8c3a6c
Clagla93 <clagla9315@gmail.com>
2024-07-29 21:02:24 -0500

    update: files, Tarea_1

--- TEMA_01_HOLA_MUNDO/CRISTHIAN_LAGLA/TAREA/hola_mundo.dart
+++ TEMA_01_HOLA_MUNDO/CRISTHIAN_LAGLA/TAREA/hola_mundo.dart
@@ -2,9 +2,21 @@
     
 /// En el codigo enviado a analizar se debe rempleazar el * inicial por la palabra void.
 /// <h2> Segundo </h2>
-
+//codigo error
     /*
     *main() {
         println("Hello World")
     }
+    */
+
+ // codigo correcto
+
+ void main() {
+        print("Hello World");
+    }
+
+    /*
+    Se retira * por void
+    Se quita ln en el print 
+    Se aumenta ; al final del print
     */
