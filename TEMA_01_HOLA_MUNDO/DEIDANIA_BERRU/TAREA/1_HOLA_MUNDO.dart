a1b8b19e22d05ba6a83177a259be3ab09768956f
deidaniatest <deidania2@gmail.com>
2024-07-29 20:55:41 -0500

    add: files ,1_hola_mundo, 2_comentarios, 3_HOLA_MUNDO

---
+++ TEMA_01_HOLA_MUNDO/DEIDANIA_BERRU/TAREA/1_HOLA_MUNDO.dart
@@ -0,0 +1,25 @@
+
+/* 
+fuertemente tipado
+todo es un objeto
+gestion automatica de memoria
+permite documentacion
+permite documentar  para ayudas al desarrollador
+permite completar
+*/
+/* 2. Analisis
+ ```dart
+    *main() {
+        println("Hello World")
+    }
+```
+ 1.- *no existe
+ 2.- para una mejor claridad se debe utilizar el  void
+ 3.- println no existe en dart el comanto correcto es dart
+ 4.- falte el ; despues de cada sentencia
+ el codigo correcto es
+*/
+void main()
+{
+  print("Hola mundo");
+}
