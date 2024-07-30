299ad40a310a026796c000a7fdefabe49aa37a50
Joelintriago <david.intriagom@ug.edu.ec>
2024-07-29 21:11:06 -0500

    add: files, tarea hola_mundo.dart

--- TEMA_01_HOLA_MUNDO/JOEL_INTRIAGO/TAREA/hola_mundo.dart
+++ TEMA_01_HOLA_MUNDO/JOEL_INTRIAGO/TAREA/hola_mundo.dart
@@ -7,5 +7,22 @@ todas las características que encontramos en el lenguaje dart:
 
 CARACTERÍSTICA:
 -Fuertemente tipado.
-
+-Todo es un objeto
 */
+
+/*
+En el siguiente código. ¿Que está incorrecto?
+
+```dart
+    *main() {
+        println("Hello World")
+    }
+```
+* En dart no existe
+println en dart no existe
+El punto y coma al final (;)
+*/
+
+main() {
+    print("Hello World");
+}
