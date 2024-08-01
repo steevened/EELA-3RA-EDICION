ba6c9f5fb2cca1cc5e9cf4971b4100e7f2bd7743
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-08-01 18:51:58 -0500

    chore: Add code for logical operators and arithmetic operators

--- TEMA_02_VARIABLES/ALEX_MOROCHO/TAREA/variables.dart
+++ TEMA_02_VARIABLES/ALEX_MOROCHO/TAREA/variables.dart
@@ -1,13 +1,13 @@
 void main() {
  //   / final
-    final valor1 = "Es un string";
-    final int valor2 = "Es un número";
-    // const
-    const pi = 5;
-    pi = 3.3;
-    // var
-    var String balance = 20.20;
-    balance = 10,1; 
+    // final valor1 = "Es un string";
+    // final int valor2 = "Es un número";
+    // // const
+    // const pi = 5;
+    // pi = 3.3;
+    // // var
+    // var String balance = 20.20;
+    // balance = 10,1; 
 }
 //El codigo intenta asignar un string a una variable  int
 //Se  reasignar un valor a una constante,  esta mal
