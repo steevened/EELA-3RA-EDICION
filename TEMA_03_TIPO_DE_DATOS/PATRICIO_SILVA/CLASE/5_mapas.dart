06b29bad777edf4f45d27c4602584ee35ec396c0
xpsimport <gerente@xpsimport.com>
2024-08-02 11:18:11 -0500

    add: files tarea tema 3 tipo de datos

--- TEMA_03_TIPO_DE_DATOS/PATRICIO_SILVA/CLASE/5_mapas.dart
+++ TEMA_03_TIPO_DE_DATOS/PATRICIO_SILVA/CLASE/5_mapas.dart
@@ -1,6 +1,7 @@
 void main() {
 //Mapas - Map
 //map<String,int>
+  // ignore: unused_local_variable
   Map<String, int> mapa = {};
 // Map<String,String>mapa={};
 // Map<int,int>mapa={}'
@@ -9,6 +10,7 @@ void main() {
   ///Establecer el tipo de dato
   ///1. final Map<String,int>mapa1
   ///2. <String, int>{};
+  // ignore: unused_local_variable
   final Map<String, int> map1 = <String, int>{};
 
   ///Clave y el valor en la definicion de las variables
@@ -31,40 +33,41 @@ void main() {
   };
   print(usuario);
 
-/// 1. Anadir  add All
-/// Debo ingresar el apellido
-/// 
-final Map<String,dynamic>apellido={
-"apellido":"Silva",
-};
+  /// 1. Anadir  add All
+  /// Debo ingresar el apellido
+  ///
+  final Map<String, dynamic> apellido = {
+    "apellido": "Silva",
+  };
 // otra forma de agregar con una variable
-usuario.addAll(apellido);
+  usuario.addAll(apellido);
 /*usuario.addAll({
   "apellido": "Silva"
 });*/
 //otra forma de agregar
-usuario.addEntries(apellido.entries);
-print(usuario);
-/// Acceder a un valor 
-final edad=usuario["edad"];
-print(edad);
-final apellido1=usuario["apellido"];
-print(apellido1);
+  usuario.addEntries(apellido.entries);
+  print(usuario);
 
-/// puedo modificar valores
-usuario["user_name"]="xpsimport";
-print(usuario);
+  /// Acceder a un valor
+  final edad = usuario["edad"];
+  print(edad);
+  final apellido1 = usuario["apellido"];
+  print(apellido1);
 
-/// Dart tiene la posibilidad de validar si un llave existe la actualiza
-/// Si no existe, lo crea
-print(usuario["nombre"]);
-usuario["nombre"]="Byron";
-print(usuario["nombre"]);
-print(usuario);
+  /// puedo modificar valores
+  usuario["user_name"] = "xpsimport";
+  print(usuario);
 
-usuario["nobre"]="Diaz";
-print(usuario);
+  /// Dart tiene la posibilidad de validar si un llave existe la actualiza
+  /// Si no existe, lo crea
+  print(usuario["nombre"]);
+  usuario["nombre"] = "Byron";
+  print(usuario["nombre"]);
+  print(usuario);
 
-usuario.remove("nobre");
-print(usuario);
+  usuario["nobre"] = "Diaz";
+  print(usuario);
+
+  usuario.remove("nobre");
+  print(usuario);
 }
