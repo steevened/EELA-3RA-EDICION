7d2203f3fa7cd8cbc278ea2533c7a15eccf9ce2b
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-08-14 21:05:25 -0500

    feat: Add Animal class and related functionality

--- TEMA_07_POO/00_RESOURCES/CLASE/1_poo.dart
+++ TEMA_07_POO/00_RESOURCES/CLASE/1_poo.dart
@@ -1,85 +1,65 @@
+import '1_1_animal.dart';
+
 void main() {
   //// Crear objetos
   /// 1. Perro
   /// 2. Gato
   /// 3. Elefante
 
-  final perro =
-      Animal(edad: 12, genero: "Macho", color: "Blaco", tipoDeComida: 'Carne');
+  final perro = Animal(
+    "Perrito",
+    edad: 12,
+    genero: "Macho",
+    color: "Blaco",
+    tipoDeComida: 'Carne',
+  );
 
   print(perro.genero);
-  perro.comer();
+  print(perro.nombreAnimal);
+  print(perro.edad);
+  // Funciones (valor) - pasas un parámetro
+  // Setters = 12 - asignas un valor
+  perro.cumple = 100;
+  print(perro.edad);
+  perro.edad = 12;
+  print(perro.edad);
 
   final gato = Animal(
-      edad: 10, genero: "Hembra", color: "Amarillos", tipoDeComida: 'Pez');
+    "Gatito",
+    edad: 10,
+    genero: "Hembra",
+    color: "Amarillos",
+    tipoDeComida: 'Pez',
+  );
   print(gato.sumar(1, 1));
 
-  final elefante =
-      Animal(edad: 4, genero: "Macho", color: "Gris", tipoDeComida: 'Hierba');
+  final elefante = Animal(
+    "Elefante",
+    edad: 4,
+    genero: "Macho",
+    color: "Gris",
+    tipoDeComida: 'Hierba',
+  );
 
   final mono = Animal(
+    "Mono",
     color: 'Café',
     edad: 10,
     genero: 'Hembra',
-    tipoDeComida: 'Frutas',
+    tipoDeComida: 'frutas',
   );
 
   final laSumaEs = mono.sumar(12, 10);
   print(laSumaEs);
   print(mono.toString());
-}
 
-// Crear clase
-// class -> palabra reservada
-// Nombre de la clase -> PascalCase -> Primera letra mayúscula
-class Animal {
-  /// Atributos
-  /// Argumentos dentro de la clase
-  /// 1. Mutable
-  int edad;
+  print(mono.tipoDeComida.toUpperCase());
+  print(mono.tipoDeComida.toUpperCase());
+  print(mono.tipoDeComida.toUpperCase());
+  print(mono.tipoDeComidaMayusculas);
+  print(mono.tipoDeComidaMayusculas);
+  print(mono.tipoDeComida.toUpperCase());
+  print(mono.tipoDeComida.toUpperCase());
 
-  /// 2. Inmutables
-  final String genero;
-  final String color;
-
-  final String tipoDeComida;
-
-  /// Constructor
-  /// Define con el nombre de la case y seguido por los atributos
-  Animal({
-    required this.edad,
-    required this.genero,
-    required this.color,
-    required this.tipoDeComida,
-  });
-
-  /// Métodos o funciones
-
-  void caminar() {
-    print("Está caminado");
-  }
-
-  void comer() {
-    print("Está comiendo");
-  }
-
-  int sumar(int valor1, int valor2) {
-    return valor1 + valor2;
-  }
-
-  /// Override -> sobreescritura
-  @override
-  String toString() {
-    return {
-      'edad': this.edad,
-      'genero': this.genero,
-    }.toString();
-  }
-
-  // copyWith 
-  // toMap
-  // toJson
-  // FromMap
-
-  /// Getters y Setters
+  print(mono.edad);
 }
